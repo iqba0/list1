e0eb2cb863e11279699f5e6febee81f12136f708
iqba0 <iqblns@gmail.com>
2026-09-23 10:35:43 +0700

    ada html baru lagi

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+n = int(input("Masukkan n barang"))
+
+daftar_barang = []
+for i in range(1,n+1):
+    barang = input(f"barang ke-{i}")
+    daftar_barang.append(barang)
+
+print(daftar_barang)
