1fcd3a9b7ff4295f04056b67c3feff82e1ca000a
iqba0 <iqblns@gmail.com>
2026-09-24 09:40:01 +0700

    Egan diganti oleh Evan

--- list.py
+++ list.py
@@ -1,25 +1,38 @@
-# #Mengubah elemen list
-daftar = [23, 55, 66, 77]
-daftar[1] = 67
-print(daftar)
+daftar_angka = []
+print(daftar_angka)
 
-# #Menambahkan elemen ke akhir list
-daftar.append(555)
-print(daftar)
 
-# #Menambahkan elemen ke indeks tertentu
-daftar.insert(0, "Ashiaaap-men")
-print(daftar)
 
-#Slicing
-daftar_baru = ["Buah", "Bayam", "Sayur", "Minuman"]
-print(daftar_baru[0:4])
 
-nilai = [80, 99, 85, 75, 65]
-print(sum(nilai)/ len(nilai))
-print(len(nilai))
-nilai.sort()
-daftar_baru.sort()
-print(daftar_baru)
+
+
+
+
+
+
+
+# # #Mengubah elemen list
+# daftar = [23, 55, 66, 77]
+# daftar[1] = 67
+# print(daftar)
+
+# # #Menambahkan elemen ke akhir list
+# daftar.append(555)
+# print(daftar)
+
+# # #Menambahkan elemen ke indeks tertentu
+# daftar.insert(0, "Ashiaaap-men")
+# print(daftar)
+
+# #Slicing
+# daftar_baru = ["Buah", "Bayam", "Sayur", "Minuman"]
+# print(daftar_baru[0:4])
+
+# nilai = [80, 99, 85, 75, 65]
+# print(sum(nilai)/ len(nilai))
+# print(len(nilai))
+# nilai.sort()
+# daftar_baru.sort()
+# print(daftar_baru)
 
 
